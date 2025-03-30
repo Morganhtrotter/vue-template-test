8f378bc79a77bf0e1e185a26265ccc87469e3c56
Morgan Trotter <35540710+Morganhtrotter@users.noreply.github.com>
2025-03-29 17:53:28 -0700

    Update vite.config.js base url

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/vue-gh-pages-template/',
+  base: '/vue-template-test/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
